483e90d1de1e7aa3f79404c71f8adc09e83ce9de
Greg Albrecht <github.com@undef.net>
2019-01-29 11:36:52 -0800

    ran through the linter

--- orion.js
+++ orion.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /*
 Orion Node-RED Nodes.
 
@@ -5,22 +6,19 @@ Author:: Greg Albrecht <gba@orionlabs.io>
 Copyright:: Copyright 2019 Orion Labs, Inc.
 License:: Apache License, Version 2.0
 Source:: https://github.com/orion-labs/node-red-contrib-orion
-
 */
 
-/*jslint node: true */
-/*jslint white: true */
+/* jslint node: true */
+/* jslint white: true */
 
-"use strict";
+'use strict';
 
 var request = require('request');
 var es = require('event-stream');
 var JSONStream = require('JSONStream');
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 var orion = require('./orionlib.js');
 
-module.exports = function (RED) {
-
+module.exports = function(RED) {
   /*
   OrionConfig
     Meta-Node for containing other Node-level configurations.
@@ -29,14 +27,14 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, config);
     this.username = config.username;
     this.password = config.password;
-    this.group_ids = config.group_ids;
+    this.groupIds = config.groupIds;
   }
 
   RED.nodes.registerType('orion_config', OrionConfig, {
     credentials: {
-      username: { type: 'text' },
-      password: { type: 'password' }
-    }
+      username: {type: 'text'},
+      password: {type: 'password'},
+    },
   });
 
   /*
@@ -49,32 +47,34 @@ module.exports = function (RED) {
     node.orion_config = RED.nodes.getNode(config.orion_config);
     node.username = node.orion_config.credentials.username;
     node.password = node.orion_config.credentials.password;
-    var group_ids = node.orion_config.group_ids.split(',');
+    var groupIds = node.orion_config.groupIds.split(',');
 
     node.status({fill: 'yellow', shape: 'dot', text: 'Idle'});
 
-    node.on('input', function (msg) {
-      var lyre_options = {
+    node.on('input', function(msg) {
+      var lyreOptions = {
         'username': node.username,
         'password': node.password,
-        'group_ids': msg.hasOwnProperty('group_ids') ? msg.group_ids : group_ids,
+        'group_ids': msg.hasOwnProperty('groupIds') ? msg.groupIds : groupIds,
         'message': msg.hasOwnProperty('message') ? msg.message : null,
         'media': msg.hasOwnProperty('media') ? msg.media : null,
-        'target': msg.hasOwnProperty('target') ? msg.target : null
+        'target': msg.hasOwnProperty('target') ? msg.target : null,
       };
       node.status({fill: 'green', shape: 'dot', text: 'Transmitting'});
-      orion.lyre(lyre_options);
+      orion.lyre(lyreOptions);
       node.status({fill: 'yellow', shape: 'dot', text: 'Idle'});
     });
 
-    node.on('close', function () { return; });
+    node.on('close', function() {
+      return;
+    });
   }
 
   RED.nodes.registerType('orion_tx', OrionTXNode, {
     credentials: {
-      username: { type: 'text' },
-      password: { type: 'password' }
-    }
+      username: {type: 'text'},
+      password: {type: 'password'},
+    },
   });
 
   /*
@@ -83,117 +83,112 @@ module.exports = function (RED) {
   */
   function OrionRXNode(config) {
       RED.nodes.createNode(this, config);
-      var node = this,
-          EventStream,
-          session_id;
+      var node = this;
+      var EventStream;
+      var sessionId;
 
       node.orion_config = RED.nodes.getNode(config.orion_config);
       node.username = node.orion_config.credentials.username;
       node.password = node.orion_config.credentials.password;
-      var group_ids = node.orion_config.group_ids.split(',');
+      var groupIds = node.orion_config.groupIds.split(',');
 
-      var es_url = 'https://api.orionlabs.io/api/ptt/' + group_ids.join('+');
-      node.debug('es_url=' + es_url);
+      var esURL = 'https://api.orionlabs.io/api/ptt/' + groupIds.join('+');
+      node.debug('esURL=' + esURL);
 
       node.status({fill: 'red', shape: 'dot', text: 'Disconnected'});
 
       // Called for every Event received from the Event Stream:
-      function event_callback (data) {
+      function eventCallback(data) {
         node.status({fill: 'green', shape: 'dot', text: 'Receiving'});
         node.send(data);
         node.status({fill: 'yellow', shape: 'dot', text: 'Idle'});
       }
 
       // Called after Auth to connect to Event Stream:
-      function event_stream_callback (auth) {
+      function eventStreamCallback(auth) {
         if (auth.error) {
           console.error(auth.error);
-          node.status({
-              fill: 'red', shape: 'dot', text: auth.error
-          });
+          node.status({fill: 'red', shape: 'dot', text: auth.error});
         } else if (!auth.token) {
           console.error('No Auth Token');
-          node.status({
-              fill: 'red', shape: 'dot', text: 'No Auth Token'
-          });
+          node.status({fill: 'red', shape: 'dot', text: 'No Auth Token'});
         } else if (auth.token && auth.sessionId) {
-          session_id = auth.sessionId;
+          sessionId = auth.sessionId;
 
-          orion.engage(auth.token, group_ids);
+          orion.engage(auth.token, groupIds);
           node.status({fill: 'green', shape: 'dot', text: 'Engaged'});
 
-          var es_options = {
-            url: es_url,
+          var esOptions = {
+            url: esURL,
             method: 'GET',
-            headers: { 'Authorization': auth.token },
-            timeout: 120000
+            headers: {'Authorization': auth.token},
+            timeout: 120000,
           };
 
           node.debug('Connecting to Event Stream.');
 
-          EventStream = request(es_options, function(error) {
+          EventStream = request(esOptions, function(error) {
             if (error) {
               node.error('error.code=' + error.code);
               node.error(error.code === 'ETIMEDOUT');
 
-              // Set to `true` if the timeout was a connection timeout, `false` or
-              // `undefined` otherwise.
+              // Set to `true` if the timeout was a connection timeout,
+              // `false` or `undefined` otherwise.
               node.error('error.connect=' + error.connect);
               node.error(error.connect === true);
 
-              node.status({
-                  fill: 'red', shape: 'dot', text: error
-              });
+              node.status({fill: 'red', shape: 'dot', text: error});
             } else {
               node.error('In error function but no error.');
 
               node.status({
-                  fill: 'yellow', shape: 'dot', text: 'Unknown State'
+                  fill: 'yellow', shape: 'dot', text: 'Unknown State',
               });
             }
           });
 
           EventStream.pipe(JSONStream.parse()).pipe(es.mapSync(
-            function (data) {
+            function(data) {
               if (data.event_type === 'ping') {
                 node.debug('Ping Received.');
                 // Respond to Engage's Ping/Pong
                 orion.pong(auth.token)
-                  .then(function (response) {
+                  .then(function(response) {
                     node.debug('Pong succeeded.');
                     node.status({fill: 'green', shape: 'dot', text: 'Engaged'});
-                    event_callback(data);
+                    eventCallback(data);
                   })
-                  .catch(function (response) {
+                  .catch(function(response) {
                     node.debug('Pong failed, calling engage()');
-                    node.status({fill: 'yellow', shape: 'dot', text: 'Re-engaging'});
-                    orion.engage(auth.token, group_ids);
-                    event_callback(data);
+                    node.status(
+                      {fill: 'yellow', shape: 'dot', text: 'Re-engaging'});
+                    orion.engage(auth.token, groupIds);
+                    eventCallback(data);
                   });
               } else {
-                event_callback(data);
+                eventCallback(data);
               }
             }
           ));
         }
     }
 
-    orion.auth(node.username, node.password, event_stream_callback);
+    orion.auth(node.username, node.password, eventStreamCallback);
 
     node.on('close', function() {
       node.debug('Closing');
       EventStream.abort();
-      orion.logout(session_id);
+      orion.logout(sessionId);
       node.status({fill: 'red', shape: 'dot', text: 'Disconnected'});
     });
   }
 
   RED.nodes.registerType('orion_rx', OrionRXNode, {
     credentials: {
-      username: { type: 'text' },
-      password: { type: 'password' },
-      group_ids: { type: 'text' }
-    }
+      username: {type: 'text'},
+      password: {type: 'password'},
+      groupIds: {type: 'text'},
+    },
   });
 
   /*
@@ -206,11 +201,11 @@ module.exports = function (RED) {
 
     node.status({fill: 'yellow', shape: 'dot', text: 'Idle'});
 
-    function wav2ov_cb (response) {
+    function wav2ovCallback(response) {
       node.send(response);
     }
 
-    node.on('input', function (msg) {
+    node.on('input', function(msg) {
       if (msg.hasOwnProperty('media_wav')) {
         node.warn('DEPRECATED: Using "media_wav", use "payload" instead.');
       }
@@ -219,9 +214,10 @@ module.exports = function (RED) {
         node.warn('DEPRECATED: Using "media_buf", use "payload" instead.');
       }
 
-      if (msg.hasOwnProperty('media_wav') || msg.hasOwnProperty('media_buf') || msg.hasOwnProperty('payload')) {
+      if (msg.hasOwnProperty('media_wav') || msg.hasOwnProperty('media_buf') ||
+          msg.hasOwnProperty('payload')) {
         node.status({fill: 'green', shape: 'dot', text: 'Encoding'});
-        orion.locris_wav2ov(msg, wav2ov_cb);
+        orion.locrisWAV2OV(msg, wav2ovCallback);
         node.status({fill: 'yellow', shape: 'dot', text: 'Idle'});
       } else {
         node.send(msg);
@@ -241,15 +237,15 @@ module.exports = function (RED) {
 
     node.status({fill: 'yellow', shape: 'dot', text: 'Idle'});
 
-    function ov2wav_cb (response) {
+    function ov2wavCallback(response) {
       node.send(response);
     }
 
-    node.on('input', function (msg) {
+    node.on('input', function(msg) {
       if (msg.hasOwnProperty('event_type') && msg.event_type === 'ptt') {
         node.status({fill: 'green', shape: 'dot', text: 'Decoding'});
         msg.return_type = config.return_type;
-        orion.locris_ov2wav(msg, ov2wav_cb);
+        orion.locrisOV2WAV(msg, ov2wavCallback);
         node.status({fill: 'yellow', shape: 'dot', text: 'Idle'});
       } else {
         node.send(msg);
@@ -274,25 +270,27 @@ module.exports = function (RED) {
 
     node.status({fill: 'yellow', shape: 'dot', text: 'Idle'});
 
-    function lookup_cb (data) {
+    function LookupCallback(data) {
       node.status({fill: 'blue', shape: 'dot', text: 'Lookup'});
       node.send(data);
       node.status({fill: 'yellow', shape: 'dot', text: 'Idle'});
     }
 
-    node.on('input', function (msg) {
-      orion.auth(node.username, node.password, function (auth) {
-        orion.lookup(auth, msg, lookup_cb);
+    node.on('input', function(msg) {
+      orion.auth(node.username, node.password, function(auth) {
+        orion.lookup(auth, msg, LookupCallback);
       });
     });
 
-    node.on('close', function () { return; });
+    node.on('close', function() {
+      return;
+    });
   }
 
   RED.nodes.registerType('orion_lookup', OrionLookup, {
     credentials: {
-      username: { type: 'text' },
-      password: { type: 'password' }
-    }
+      username: {type: 'text'},
+      password: {type: 'password'},
+    },
   });
 };
--- orionlib.js
+++ orionlib.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /*
 Orion Node.JS Library
 
@@ -9,25 +10,26 @@ Source:: https://github.com/orion-labs/node-red-contrib-orion
 */
 
 
-/*jslint node: true */
-/*jslint white: true */
+/* jslint node: true */
+/* jslint white: true */
 
-"use strict";
+'use strict';
 
 var request = require('request');
-var JSONStream = require('JSONStream');
-var es = require('event-stream');
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
+var LYRE_URL = 'https://lyre.api.orionaster.com/lyre';
+var OCHRE_URL = 'https://ochre.api.orionaster.com/ochre';
+var LOCRIS_OV2WAV = 'https://locris.api.orionaster.com/ov2wav';
+var LOCRIS_WAV2OV = 'https://locris.api.orionaster.com/wav2ov';
 
 // Login to Orion and retrieve a Auth Token, then call callback:
-function auth (username, password, callback) {
+function auth(username, password, callback) {
   request({
     url: 'https://api.orionlabs.io/api/login',
     method: 'POST',
-    json: { 'uid': username, 'password': password }
-  }, function (error, response, body) {
-
+    json: {'uid': username, 'password': password},
+  }, function(error, response, body) {
     if (error) {
       callback({error: error});
     } else if (response.statusCode !== 200) {
@@ -35,19 +37,18 @@ function auth (username, password, callback) {
     } else if (response.statusCode === 200) {
       callback(body);
     }
-
   });
 }
 exports.auth = auth;
 
 // Login to Orion and retrieve a Auth Token for later use:
-function auth_promise (username, password) {
+function authPromise(username, password) {
   return new Promise(function(resolve, reject) {
     request({
       url: 'https://api.orionlabs.io/api/login',
       method: 'POST',
-      json: { 'uid': username, 'password': password }
-    }, function (error, response, body) {
+      json: {'uid': username, 'password': password},
+    }, function(error, response, body) {
       if (error) {
         reject(error);
       } else {
@@ -56,13 +57,13 @@ function auth_promise (username, password) {
     });
   });
 }
-exports.auth_promise = auth_promise;
+exports.authPromise = authPromise;
 
 
-function logout (session_id) {
+function logout(sessionId) {
   request({
-    url: 'https://api.orionlabs.io/api/logout/' + session_id,
-    method: 'POST'
+    url: 'https://api.orionlabs.io/api/logout/' + sessionId,
+    method: 'POST',
   });
 }
 exports.logout = logout;
@@ -71,21 +72,18 @@ exports.logout = logout;
 'Engage' with the Orion Event Stream.
 Ensures user Presence for asyncronous stream connections (APN).
 */
-function engage (token, group_ids) {
-  console.log(Date() + ' engage() group_ids=' + group_ids);
+function engage(token, groupIds) {
+  console.log(Date() + ' engage() groupIds=' + groupIds);
 
-  var engage_options = {
+  var engageOptions = {
     url: 'https://api.orionlabs.io/api/engage',
     method: 'POST',
-    headers: { 'Authorization': token },
+    headers: {'Authorization': token},
     json: {
       seqnum: Date.now(),
-      groupIds: group_ids,
-      destinations: [{
-        destination: 'EventStream',
-        verbosity: 'active'
-      }]
-    }
+      groupIds: groupIds,
+      destinations: [{destination: 'EventStream', verbosity: 'active'}],
+    },
   };
 
   /*
@@ -94,18 +92,18 @@ function engage (token, group_ids) {
 
   The prescribed period is 5 minutes, or 300000 ms.
   */
-  var engageTimer = setTimeout(function () {
+  var engageTimer = setTimeout(function() {
     console.log(Date() + ' Engage Timeout.');
-    engage (token, group_ids);
+    engage(token, groupIds);
   }, 360000);
 
-  function engage_callback (error, response, body) {
+  function engageCallback(error, response, body) {
     if (error) {
       console.log(Date() + ' Unable to Engage. error=' + error);
     } else if (response.statusCode === 409) {
       console.log(Date() + ' Re-engaging.');
       clearTimeout(engageTimer);
-      engage (token, group_ids);
+      engage(token, groupIds);
     } else if (!error && response.statusCode === 200) {
       console.log(Date() + ' Engaged.');
     } else {
@@ -113,20 +111,20 @@ function engage (token, group_ids) {
     }
   }
 
-  request(engage_options, engage_callback);
+  request(engageOptions, engageCallback);
 }
 exports.engage = engage;
 
 
 // Respond to an Event Stream Engage Ping
-function pong (token, ping_id) {
-  console.log(Date() + ' pong() ping_id=' + ping_id);
+function pong(token, pingId) {
+  console.log(Date() + ' pong() pingId=' + pingId);
   return new Promise(function(resolve, reject) {
     request({
       url: 'https://api.orionlabs.io/api/pong',
       method: 'POST',
-      headers: { 'Authorization': token }
-    }, function (err, response, body) {
+      headers: {'Authorization': token},
+    }, function(err, response, body) {
       if (err) {
         reject(response);
       } else {
@@ -139,49 +137,48 @@ exports.pong = pong;
 
 
 // Orion TTS-as-a-Service. (TTSAAS?)
-function lyre (options) {
-  var lyre_url = process.env.LYRE_URL || 'https://lyre.api.orionaster.com/lyre';
-  console.debug(Date() + ' lyre_url=' + lyre_url);
+function lyre(options) {
+  var lyreUrl = process.env.LYRE_URL || LYRE_URL;
+  console.debug(Date() + ' lyreUrl=' + lyreUrl);
 
-  auth_promise(options.username, options.password)
-    .then(function (auth) {
+  authPromise(options.username, options.password)
+    .then(function(auth) {
       request({
-        url: lyre_url,
+        url: lyreUrl,
         method: 'POST',
         json: {
           'token': auth.token,
-          'group_ids': options.group_ids,
+          'group_ids': options.groupIds,
           'message': options.message || null,
           'media': options.media || null,
-          'target': options.target || null
-        }
+          'target': options.target || null,
+        },
       },
-      function (err, response, body) {
+      function(err, response, body) {
         if (err) {
           console.error(err);
         }
       });
     });
-
 }
 exports.lyre = lyre;
 
 
-function lookup_promise (options, callback) {
-  var ochre_url = process.env.OCHRE_URL || 'https://ochre.api.orionaster.com/ochre';
+function lookupPromise(options, callback) {
+  var ochreUrl = process.env.OCHRE_URL || OCHRE_URL;
 
-    auth_promise(options.username, options.password)
-      .then(function (auth) {
+    authPromise(options.username, options.password)
+      .then(function(auth) {
         request({
-          url: ochre_url,
+          url: ochreUrl,
           method: 'POST',
           json: {
             'token': auth.token,
             'user_id': auth.id,
-            'msg': options.msg
-          }
+            'msg': options.msg,
+          },
         },
-        function (error, response, body) {
+        function(error, response, body) {
           if (error) {
             console.log(Date() + ' Lookup error=' + error);
           } else {
@@ -190,21 +187,21 @@ function lookup_promise (options, callback) {
         });
       });
 }
-exports.lookup_promise = lookup_promise;
+exports.lookupPromise = lookupPromise;
 
 
-function lookup (auth, msg, callback) {
-  var ochre_url = process.env.OCHRE_URL || 'https://ochre.api.orionaster.com/ochre';
+function lookup(auth, msg, callback) {
+  var ochreUrl = process.env.OCHRE_URL || OCHRE_URL;
   request({
-    url: ochre_url,
+    url: ochreUrl,
     method: 'POST',
     json: {
       'token': auth.token,
       'user_id': auth.id,
-      'msg': msg
-    }
+      'msg': msg,
+    },
   },
-  function (error, response, body) {
+  function(error, response, body) {
     if (error) {
       console.log(Date() + ' Lookup error=' + error);
       console.log(response);
@@ -216,17 +213,18 @@ function lookup (auth, msg, callback) {
 exports.lookup = lookup;
 
 
-function locris_wav2ov (msg, callback) {
+function locrisWAV2OV(msg, callback) {
   // Override wav2ov endpoint for local development:
-  var locris_wav2ov_url = process.env.LOCRIS_WAV2OV || 'https://locris.api.orionaster.com/wav2ov',
-      xhr = new XMLHttpRequest();
-  console.debug(Date() + ' locris_wav2ov_url=' + locris_wav2ov_url);
+  var locrisWAV2OVURL = process.env.LOCRIS_WAV2OV || LOCRIS_WAV2OV;
+  var xhr = new XMLHttpRequest();
 
-  xhr.open('POST', locris_wav2ov_url, true);
+  console.debug(Date() + ' locrisWAV2OVURL=' + locrisWAV2OVURL);
+
+  xhr.open('POST', locrisWAV2OVURL, true);
 
   xhr.setRequestHeader('Content-Type', 'application/json');
 
-  xhr.onreadystatechange = function () {
+  xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
       var response = JSON.parse(xhr.responseText);
       callback(response);
@@ -235,19 +233,20 @@ function locris_wav2ov (msg, callback) {
 
   xhr.send(JSON.stringify(msg));
 }
-exports.locris_wav2ov = locris_wav2ov;
+exports.locrisWAV2OV = locrisWAV2OV;
 
 
-function locris_ov2wav (msg, callback) {
-  var locris_ov2wav_url = process.env.LOCRIS_OV2WAV || 'https://locris.api.orionaster.com/ov2wav',
-      xhr = new XMLHttpRequest();
-  console.debug(Date() + ' locris_ov2wav_url=' + locris_ov2wav_url);
+function locrisOV2WAV(msg, callback) {
+  var locrisOV2WAVURL = process.env.LOCRIS_OV2WAV || LOCRIS_OV2WAV;
+  var xhr = new XMLHttpRequest();
 
-  xhr.open('POST', locris_ov2wav_url, true);
+  console.debug(Date() + ' locrisOV2WAVURL=' + locrisOV2WAVURL);
+
+  xhr.open('POST', locrisOV2WAVURL, true);
 
   xhr.setRequestHeader('Content-Type', 'application/json');
 
-  xhr.onreadystatechange = function () {
+  xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
       var response = JSON.parse(xhr.responseText);
 
@@ -260,4 +259,4 @@ function locris_ov2wav (msg, callback) {
 
   xhr.send(JSON.stringify(msg));
 }
-exports.locris_ov2wav = locris_ov2wav;
+exports.locrisOV2WAV = locrisOV2WAV;
